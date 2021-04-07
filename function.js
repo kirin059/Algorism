737a8d1c99f088381e85668279207a1592dbea3a
saebom <boma91@gmail.com>
2021-04-08 01:56:22 +0900

    Add: maximun number 구하기

--- function.js
+++ function.js
@@ -149,3 +149,14 @@ function changeStr(n) {
     console.log(empty.join(''));
 }
 changeStr(strs)
+
+
+// 19. 순서가 없는 10개의 숫자가 공백으로 구분되어 주어진다. 주어진 숫자들 중 최댓값을 반환하라.
+let random = '1 2 10 9 8 7 6 5 4 3'
+
+function maximum(n) {
+    let max = n.split(' ').sort((a, b) => b - a);
+    console.log(max[0])
+
+}
+maximum(random)
